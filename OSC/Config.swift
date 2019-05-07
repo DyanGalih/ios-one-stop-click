b203f65e02581acca8b7e6f2e61cb80f5327b124
Dyan Galih <dyan.galih@gmail.com>
2019-05-07 15:46:52 +0700

    Refactor the code into clean swift

---
+++ OSC/Config.swift
@@ -0,0 +1,13 @@
+//
+//  Config.swift
+//  OSC
+//
+//  Created by Dyan Galih on 02/05/19.
+//  Copyright © 2019 Dyan Galih. All rights reserved.
+//
+
+import Foundation
+
+class Config{
+    let endpoint = "http://172.19.13.156/api/";
+}
---
+++ Podfile
@@ -0,0 +1,13 @@
+# 1
+source 'https://github.com/CocoaPods/Specs.git'
+# 2
+platform :ios, '10.0.3'
+# 3
+use_frameworks!
+
+# 4
+target 'OSC' do
+  # 5
+  pod 'Alamofire'
+  pod "SharkORM"
+end
